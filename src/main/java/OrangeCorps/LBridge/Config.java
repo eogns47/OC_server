866bb2080defafb7c99d880935ce812f344d1084
kangdaehoon <eogns47@konkuk.ac.kr>
2023-11-11 09:46:03 +0900

    feat: Config 파일 생성
    상수를 저장하는 COnfig 파일 생성

---
+++ src/main/java/OrangeCorps/LBridge/Config.java
@@ -0,0 +1,13 @@
+package OrangeCorps.LBridge;
+
+public class Config {
+    public final static String USER_SAVE_SUCCESS = "User saved successfully: {}";
+    public final static String NOT_FOUND_USER = "User not found\n";
+    public final static String NOT_FOUND_COUPLE_USER = "coupleUser not found\n";
+    public final static String COUPLE_LINK_SUCCESS = "coupleUser not found\n";
+
+    public final static String USER_SAVE_ERROR = "Error saving user";
+
+    public final static Integer PAIR_OF_COUPLE = 2;
+
+}
