d49e1631ae08fecba1a3f15e3c8486ac1fee510d
kangdaehoon <eogns47@konkuk.ac.kr>
2023-11-13 16:57:13 +0900

    feat: config 파일 목록 추가

--- src/main/java/OrangeCorps/LBridge/Config.java
+++ src/main/java/OrangeCorps/LBridge/Config.java
@@ -10,4 +10,12 @@ public class Config {
 
     public final static Integer PAIR_OF_COUPLE = 2;
 
+    public final static String QUERY_IS = "?q=";
+
+    public final static String My_API_KEY_IS = "&api-key=";
+
+    public final static Integer NEWS_START_INDEX = 0;
+
+    public final static Integer NEWS_END_INDEX =5;
+
 }
