6967d484a0da5789c58b685972c2af1ab83162e3
kangdaehoon <eogns47@konkuk.ac.kr>
2023-11-14 12:09:12 +0900

    feat: NewsController 구현

---
+++ src/main/java/OrangeCorps/LBridge/Controller/NewsController.java
@@ -0,0 +1,23 @@
+package OrangeCorps.LBridge.Controller;
+
+
+import OrangeCorps.LBridge.DTO.NewsDTO;
+import OrangeCorps.LBridge.Service.News.NewsService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewsController {
+
+    @Autowired
+    private NewsService newsService;
+
+    @GetMapping("news")
+    public List<NewsDTO> crawlNews(@RequestParam String userId) {
+            return newsService.getTenOfNewsOfCouple(userId);
+    }
+
+}
