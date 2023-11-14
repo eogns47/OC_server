a3ce3ed9bfe062e985a569c7ba2750d5cf813da5
kangdaehoon <eogns47@konkuk.ac.kr>
2023-11-14 13:00:17 +0900

    feat: NewsAPIResponse 구현 받아온 Json 형식을 객체로 변환하기 위한 작업

---
+++ src/main/java/OrangeCorps/LBridge/DTO/NewsApiResponse.java
@@ -0,0 +1,44 @@
+package OrangeCorps.LBridge.DTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class NewsApiResponse {
+
+    @JsonProperty("response")
+    private NewsApiResponseBody response;
+
+    @Getter
+    public static class NewsApiResponseBody {
+
+        @JsonProperty("docs")
+        private List<Docs> docs;
+
+        @Getter
+        public static class Docs {
+
+            @JsonProperty("web_url")
+            private  String url;
+
+            @JsonProperty("headline")
+            private Headline headline;
+
+            @JsonProperty("abstract")
+            private String summary;
+
+            @JsonProperty("pub_date")
+            private String pubDate;
+
+            @Getter
+            public static class Headline {
+
+                @JsonProperty("main")
+                private String main;
+
+            }
+        }
+    }
+}
