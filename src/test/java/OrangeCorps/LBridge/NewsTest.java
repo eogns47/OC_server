423b85ee2380a85944f20b839346bf93576ca0f4
kangdaehoon <eogns47@konkuk.ac.kr>
2023-11-14 13:05:45 +0900

    feat: 뉴스 Test 클래스 추가

---
+++ src/test/java/OrangeCorps/LBridge/NewsTest.java
@@ -0,0 +1,19 @@
+package OrangeCorps.LBridge;
+
+import OrangeCorps.LBridge.Service.News.NewsService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NewsTest {
+    @Autowired
+    private NewsService newsService;
+
+    @Test
+    @DisplayName("API 정상 수신 테스트")
+    void IsNewsAPISuccess(){
+        newsService.getTenOfNewsOfCouple("testId");
+    }
+}
