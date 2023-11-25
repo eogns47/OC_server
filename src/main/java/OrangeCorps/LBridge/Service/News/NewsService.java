3778ac10c3f4a474b88835075c9708ec63b445b1
kangdaehoon <eogns47@konkuk.ac.kr>
2023-11-25 22:45:59 +0900

    feat: News 데이터 전송방식 변경

--- src/main/java/OrangeCorps/LBridge/Service/News/NewsService.java
+++ src/main/java/OrangeCorps/LBridge/Service/News/NewsService.java
@@ -3,21 +3,34 @@ package OrangeCorps.LBridge.Service.News;
 import static OrangeCorps.LBridge.Config.Config.My_API_KEY_IS;
 import static OrangeCorps.LBridge.Config.Config.QUERY_IS;
 
+import OrangeCorps.LBridge.Domain.News.News;
 import OrangeCorps.LBridge.Domain.News.NewsDTO;
+import OrangeCorps.LBridge.Domain.News.NewsRepository;
+import OrangeCorps.LBridge.Domain.User.User;
+import OrangeCorps.LBridge.Domain.User.UserRepository;
 import OrangeCorps.LBridge.Service.CoupleService;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
 import static OrangeCorps.LBridge.Config.Config.*;
 
 @Service
 public class NewsService {
 
     @Autowired
-    CoupleService coupleService;
+    private CoupleService coupleService;
+
+    @Autowired
+    private NewsRepository newsRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Value("${news.api.url}")
     private String apiUrl;
@@ -28,29 +41,61 @@ public class NewsService {
     @Autowired
     RestTemplate restTemplate;
 
-    public NewsDTO getNewsOfCouple(String userId,Integer idx) {
+    public NewsDTO getNewsOfCouple(Integer idx,String country) {
         NewsDTO newsDTO;
-        String coupleCountry = coupleService.findCountryOfCouple(userId);
-        String APIUrl = makeUrl(coupleCountry);
+
+        String APIUrl = makeUrl(country);
 
         GetNYTimesArticles getNYTimesArticles = new GetNYTimesArticles(restTemplate);
-        getNYTimesArticles.setOptions(APIUrl,idx);
+        getNYTimesArticles.setOptions(APIUrl, idx);
         newsDTO = getNYTimesArticles.getArticle();
-
 
         return newsDTO;
     }
 
-    public String makeUrl(String coupleCountry){
-        return apiUrl + QUERY_IS + coupleCountry + My_API_KEY_IS + apiKey;
+    public String makeUrl(String country) {
+        return apiUrl + QUERY_IS + country + My_API_KEY_IS + apiKey;
     }
 
-    public List<NewsDTO> getTenOfNewsOfCouple(String userId){
-        List<NewsDTO> newsDTOs=new ArrayList<>();
-        for(int idx=NEWS_START_INDEX; idx<NEWS_END_INDEX; idx++){
-            newsDTOs.add(getNewsOfCouple(userId,idx));
+    public List<NewsDTO> getNewsesOfCouple(String country) {
+        List<NewsDTO> newsDTOs = new ArrayList<>();
+        for (int idx = NEWS_START_INDEX; idx < NEWS_END_INDEX; idx++) {
+            newsDTOs.add(getNewsOfCouple(idx, country));
         }
         return newsDTOs;
+    }
+
+    public void saveNews() {
+        List<NewsDTO> newsDTOs = new ArrayList<>();
+
+        List<String> distinctCountries = getDistinctCountries();
+
+        for(String country : distinctCountries) {
+            newsDTOs = getNewsesOfCouple(country);
+
+            for (NewsDTO newsDTO : newsDTOs) {
+
+                // News 엔티티 생성 및 값 설정
+                News newsEntity = News.builder()
+                        .url(newsDTO.getUrl())
+                        .headLine(newsDTO.getHeadLine())
+                        .summary(newsDTO.getSummary())
+                        .published_date(newsDTO.getPublished_date())
+                        .country(country)
+                        .build();
+
+                newsRepository.save(newsEntity);
+            }
+        }
+    }
+
+    private List<String> getDistinctCountries() {
+        List<User> allUsers = userRepository.findAll();
+
+        return allUsers.stream()
+                .map(User::getCountry)
+                .distinct()
+                .collect(Collectors.toList());
     }
 
 }
