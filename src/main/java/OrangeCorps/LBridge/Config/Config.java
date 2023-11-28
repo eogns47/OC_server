9de1fb85298fe779ff8e559ed6c66d80afac902a
kangdaehoon <eogns47@konkuk.ac.kr>
2023-11-28 15:23:09 +0900

    feat: config 파일 문자열 추가

--- src/main/java/OrangeCorps/LBridge/Config/Config.java
+++ src/main/java/OrangeCorps/LBridge/Config/Config.java
@@ -1,12 +1,36 @@
 package OrangeCorps.LBridge.Config;
 
 public class Config {
+    //val
     public final static String USER_SAVE_SUCCESS = "User saved successfully: {}";
     public final static String NOT_FOUND_USER = "User not found\n";
-    public final static String NOT_FOUND_COUPLE_USER = "User Or CoupleUser not found\n";
+    public final static String NOT_FOUND_COUPLE_USER = "CoupleUser not found ";
     public final static String COUPLE_LINK_SUCCESS = "Couple Register Success\n";
 
     public final static String USER_SAVE_ERROR = "Error saving user";
+
+    public final static String VALIDATE_ERROR = "validate error occur";
+
+    //couple
+    public final static String COUPLE_REGISTRATION_REQUEST_SUCCESS = "COUPLE REGISTRATION REQUEST SUCCESSFUL";
+    public final static String COUPLE_REQUEST_EXIST = "Couple request exist from ";
+
+    public final static String COUPLE_REQUEST_NOT_EXIST = "User Dont receive Couple request";
+
+    public final static String SEND_REQUEST_NOT_EXIST = "User Dont send request";
+
+    public final static String DELETE_SUCCESSFUL = "Delete success";
+
+    public final static String DECLINE_SUCCESSFUL = "Decline success";
+
+    public final static String MY_REQUEST_ACCEPTED = "Request Accept";
+    public final static String MY_REQUEST_WAITING = "Request Waiting";
+    public final static String MY_REQUEST_DECLINED = "Request Declined";
+
+
+
+
+    //static
 
     public final static Integer PAIR_OF_COUPLE = 2;
 
